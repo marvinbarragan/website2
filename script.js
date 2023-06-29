967dd0fa785d7a0028c85027bbcf9fd5c3297e2c
marvinbarragan <138074433+marvinbarragan@users.noreply.github.com>
2023-06-29 09:44:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var button = document.getElementById("hi")
+<button.addEventListener("click", function(){>
+console.log("click");
+
+
+});
+var body = document.body;
+body.setAttribute("style", " background-color: black");
+
